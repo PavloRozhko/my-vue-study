0c7267944909c9e29295b1f71abb4507215a04a2
Pavlo Rozhko <91882494+PavloRozhko@users.noreply.github.com>
2023-10-29 09:25:31 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<script setup>
-import LessonFive from './lessons/Lesson-05.vue'
+<template>
+	<first-task />
+	<second-task />
+</template>
+
+<script>
+import FirstTask from './components/task01/FirstTask.vue';
+import SecondTask from './components/task02/SecondTask.vue'
+export default {
+	name: 'App',
+	components: {
+		FirstTask,
+		SecondTask
+	}
+}
 </script>
 
-<template>
-  <LessonFive />
-</template>
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	max-width: 1200px;
+	margin: 0 auto;
+	/* -webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px; */
+}
+</style>
